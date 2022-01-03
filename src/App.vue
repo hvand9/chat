837a021d189795d41858f592a39bf5ddbf9d65a1
hvand9 <54908812+hvand9@users.noreply.github.com>
2022-01-03 14:12:42 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <router-view/>
+   <div id="app">
+        <img src="./assets/Gamemeet.png">
+        <center><button>Tilbage til forsiden</button></center>
+    </div>
+</template>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+img {
+position: absolute;
+width: 300px;
+height: 120px;
+left: -2px;
+top: 0px;
+
+}
+
+
+
+</style>
