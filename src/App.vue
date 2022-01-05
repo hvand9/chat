0e6347e97d8b425920fa172f41ea38dfde7fdef4
hvand9 <54908812+hvand9@users.noreply.github.com>
2022-01-05 02:57:13 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
    <div id="app">
         <img src="./assets/Gamemeet.png">
         <center> <button onclick="window.location.href='https://61d482940327745607ae8534--gamemeet.netlify.app/?fbclid=IwAR2mIY_Y3C7FReT3TvyizcacckQf6eQDL-kdOJsju7UKRNWQE9ievaUNgg8'">TIlbage til forsiden</button></center>
+        <center> <button onclick="window.location.href='https://sad-perlman-9d529a.netlify.app/">Gå til profil</button></center>
+
     </div>
 </template>
 
